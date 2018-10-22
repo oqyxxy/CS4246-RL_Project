a1a1f9fa8f91119b06e021352360d817db30c167
oqyxxy <oqyxxy93@gmail.com>
2018-10-22 12:36:02 +0800

    add ref implementation

--- src/example.py
+++ src/example.py
@@ -1,6 +1,14 @@
 import gym
-env = gym.make('MsPacman-ram-v0')
-# env = gym.make('CarRacing-v0')
+
+
+ENV_NAME = 'MsPacman-ram-v0'
+
+
+env = gym.make(ENV_NAME)
+nb_actions = env.action_space.n
+
+
+
 for i_episode in range(20):
     observation = env.reset()
     for t in range(100):
