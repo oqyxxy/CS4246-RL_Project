1b4589fd51ff4d5bfeec52e1e732716e0195f5e5
oqyxxy <oqyxxy93@gmail.com>
2018-10-24 23:09:34 +0800

    Updated pacman-rl

--- src/pacman_rl.py
+++ src/pacman_rl.py
@@ -16,7 +16,7 @@ from rl.core import Processor
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
 ENV_NAME = "MsPacmanDeterministic-v4"
-INPUT_SHAPE = (80, 80)
+INPUT_SHAPE = (80, 80)  # Each frame will take 6400bytes of memory
 WINDOW_LENGTH = 4
 
 
@@ -41,29 +41,27 @@ class AtariProcessor(Processor):
         return processed_batch
 
     # def process_reward(self, reward):
-    #     if reward > 10:
-    #         print(reward)
-    #     elif reward < 0:
-    #         print(reward)
-    #     return np.clip(reward, -1., 1.)
+    #     return np.sign(reward)
+    #
+    # def process_info(self, info):
+    #     if info['ale.lives'] < 1:
+    #         print(info['ale.lives'])
+    #     return info
 
 
 def build_nn_model():
     input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
+
     model = Sequential()
     model.add(Permute((2, 3, 1), input_shape=input_shape))
     model.add(Convolution2D(32, 8, strides=4))
-    # model.add(LeakyReLU(alpha=0.3))
     model.add(Activation('relu'))
     model.add(Convolution2D(64, 4, strides=2))
-    # model.add(LeakyReLU(alpha=0.3))
     model.add(Activation('relu'))
     model.add(Convolution2D(64, 3, strides=1))
-    # model.add(LeakyReLU(alpha=0.3))
     model.add(Activation('relu'))
     model.add(Flatten())
     model.add(Dense(512))
-    # model.add(LeakyReLU(alpha=0.3))
     model.add(Activation('relu'))
     model.add(Dense(nb_actions))
     model.add(Activation('linear'))
@@ -78,56 +76,35 @@ if len(sys.argv) < 2:
 
 execution_mode = sys.argv[1]
 env = gym.make(ENV_NAME)
-# env = wrappers.Monitor(env, './', force=True)  # save animations
 nb_actions = env.action_space.n
+# env = wrappers.Monitor(env, './', force=True)  # save animations
 
 
 # Build our model with reference to deepmind DQN paper (Minh et al.).
 model = build_nn_model()
 print(model.summary())
 
-# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
-# even the metrics!
+
 memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
 processor = AtariProcessor()
 
-# Select a policy. We use eps-greedy action selection, which means that a random action is selected
-# with probability eps. We anneal eps from 1.0 to 0.1 over the course of 1M steps. This is done so that
-# the agent initially explores the environment (high eps) and then gradually sticks to what it knows
-# (low eps). We also set a dedicated eps value that is used during testing. Note that we set it to 0.05
-# so that the agent still performs some random actions. This ensures that the agent cannot get stuck.
 policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
                               nb_steps=1000000)
-
-# The trade-off between exploration and exploitation is difficult and an on-going research topic.
-# If you want, you can experiment with the parameters or use a different policy. Another popular one
-# is Boltzmann-style exploration:
-# policy = BoltzmannQPolicy(tau=1.)
-# Feel free to give it a try!
-
 dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
                processor=processor, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
                train_interval=4, delta_clip=1., enable_double_dqn=True, enable_dueling_network=True)
 dqn.compile(Adam(lr=.00025), metrics=['mae'])
 
 if execution_mode == 'train':
-    # Okay, now it's time to learn something! We capture the interrupt exception so that training
-    # can be prematurely aborted. Notice that you can the built-in Keras callbacks!
     weights_filename = 'dqn_{}_weights.h5f'.format(ENV_NAME)
     checkpoint_weights_filename = 'dqn_' + ENV_NAME + '_weights_{step}.h5f'
     log_filename = 'dqn_{}_log.json'.format(ENV_NAME)
     callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=2500000)]
     callbacks += [FileLogger(log_filename, interval=100)]
     dqn.fit(env, callbacks=callbacks, nb_steps=10000000, log_interval=1000000)
-
-    # After training is done, we save the final weights one more time.
     dqn.save_weights(weights_filename, overwrite=True)
-
-    # Finally, evaluate our algorithm for 10 episodes.
     dqn.test(env, nb_episodes=10, visualize=False)
 elif execution_mode == 'run':
     weights_filename = 'dqn_{}_weights.h5f'.format(ENV_NAME)
-    # if args.weights:
-    #     weights_filename = args.weights
     dqn.load_weights(weights_filename)
     dqn.test(env, nb_episodes=10, visualize=True)
